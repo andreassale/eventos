08376a320df9d1c754a9974cd2996adc1e63e59f
andreassale <andre.assale87@gmail.com>
2025-01-12 12:47:18 -0300

    Estructura inicial: frontend, backend y database

---
+++ backend/evento87/src/main/java/com/eventos/evento87/Evento87Application.java
@@ -0,0 +1,13 @@
+package com.eventos.evento87;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evento87Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Evento87Application.class, args);
+	}
+
+}
---
+++ backend/evento87/src/test/java/com/eventos/evento87/Evento87ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventos.evento87;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evento87ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
